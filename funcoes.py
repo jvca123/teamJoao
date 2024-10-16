2e59ee78b5b3c3cbbf45f143abf0c51807309550
jvca123 <jvca03122005@gmail.com>
2024-10-16 11:22:59 -0300

    Define posições

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,10 @@
-print("oi")
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicoes = []
+    if orientacao == 'vertical':
+        for i_linha in range(linha, linha + tamanho):
+            posicoes.append([i_linha, coluna])
+    else:
+        for i_coluna in range(coluna, coluna + tamanho):
+            posicoes.append([linha, i_coluna])
+            
+    return posicoes
