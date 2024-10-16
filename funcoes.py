c8222171b981fee38cd72b46d0c42093e00fc441
jvca123 <jvca03122005@gmail.com>
2024-10-16 11:35:54 -0300

    Definindo Frota

--- funcoes.py
+++ funcoes.py
@@ -8,3 +8,11 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
             posicoes.append([linha, i_coluna])
             
     return posicoes
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    if nome_navio in frota:
+        frota[nome_navio].append(define_posicoes(linha, coluna, orientacao, tamanho))
+    else:
+        frota[nome_navio] = [define_posicoes(linha, coluna, orientacao, tamanho)]
+    
+    return frota
